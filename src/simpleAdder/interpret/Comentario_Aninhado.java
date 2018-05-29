bbfdc17288c92e1016161263b1ca31e975cc44dc
Xr0s <italojcm@dcomp.ufs.br>
2018-05-29 00:33:56 -0300

    adição de pilha nos comentários de bloco para verificação da linha

--- src/simpleAdder/interpret/Comentario_Aninhado.java
+++ src/simpleAdder/interpret/Comentario_Aninhado.java
@@ -1,5 +1,7 @@
 package simpleAdder.interpret;
 
+import java.util.Stack;
+
 import simpleAdder.lexer.*;
 import simpleAdder.node.*;
 public class Comentario_Aninhado extends Lexer
@@ -7,18 +9,21 @@ public class Comentario_Aninhado extends Lexer
   private int count;
   private TComment comment;
   private StringBuffer text;
-  Token tokenAnterior;
+  Stack<Token> pilha_tokens = new Stack<Token>();
   // We define a constructor
   public Comentario_Aninhado(java.io.PushbackReader in)
   { super(in);
   }
   // We define a filter that recognizes nested comments.
   protected void filter(){ // if we are in the comment state
-    if(state.equals(State.COMMENT)){ // if we are just entering this state
-    	
+	  
+	  if(state.equals(State.COMMENT)){ // if we are just entering this state
+
       if(comment == null){ // The token is supposed to be a comment.
         // We keep a reference to it and set the count to one
         comment = (TComment) token;
+        pilha_tokens.clear();
+        pilha_tokens.push(token);
         text = new StringBuffer(comment.getText());
         count = 1;
         token = null; // continue to scan the input.
@@ -26,21 +31,25 @@ public class Comentario_Aninhado extends Lexer
       else { // we were already in the comment state
         text.append(token.getText()); // accumulate the text.
         if(token instanceof TComment){
-          count++;
-          tokenAnterior = token;
+        	
+        	pilha_tokens.push(token);
+            count++;
+          
       	}else if(token instanceof TCommentEnd){
         	count--;
+        	pilha_tokens.pop();
         }
         if(token instanceof EOF) {
-        	lancarExcecao("",tokenAnterior);
+        	lancarExcecao("",pilha_tokens.elementAt(0));
+        	pilha_tokens.clear();
         	System.exit(1);        	
         }
         
         
-        if(count != 0)
+        if(count != 0) {
           token = null; // continue to scan the input.
-        else { 
-
+        }
+        else {
 //          comment.setText(text.toString());
           token = comment; //return a comment with the full text.
           state = State.NORMAL; //go back to normal.
@@ -48,6 +57,7 @@ public class Comentario_Aninhado extends Lexer
         }
       }
     }
+    
   }
   
   public void lancarExcecao(String mensagem_extra, Token token_especial) {
